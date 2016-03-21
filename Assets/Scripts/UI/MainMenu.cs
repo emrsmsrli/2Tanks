2e5a2ac83a62be4cdf454a77d30046fa8c41c0e7
Emre <emrsmsrli1@gmail.com>
2016-03-21 19:33:10 +0200

    mainmenu: color calculation refactor

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -19,12 +19,6 @@ public class MainMenu : MonoBehaviour {
     public Slider blue1;
     public Slider blue2;
 
-
-    [HideInInspector]
-    public Color tc1;
-    [HideInInspector]
-    public Color tc2;
-
     public GameObject tank1;
     public GameObject tank2;
 
@@ -34,8 +28,7 @@ public class MainMenu : MonoBehaviour {
 
     void Awake() {
         colors = new Color[2];
-        colors[0] = tc1;
-        colors[1] = tc2;
+        calculateColors();
         colorbuttonselected = false;
 
 
@@ -51,13 +44,14 @@ public class MainMenu : MonoBehaviour {
 
     void Update() {
         if(colorbuttonselected) {
+            calculateColors();
             MeshRenderer[] t1 = tank1.GetComponentsInChildren<MeshRenderer>();
             MeshRenderer[] t2 = tank2.GetComponentsInChildren<MeshRenderer>();
             foreach(MeshRenderer mr in t1) {
-                mr.material.color = new Color(red1.value / 255, green1.value / 255, blue1.value / 255);
+                mr.material.color = colors[0];
             }
             foreach(MeshRenderer mr in t2) {
-                mr.material.color = new Color(red2.value / 255, green2.value / 255, blue2.value / 255);
+                mr.material.color = colors[1];
             }
         }
     }
@@ -81,8 +75,6 @@ public class MainMenu : MonoBehaviour {
 
 
     public void onStartClick() {
-        if(colorbuttonselected)
-            calculateColors();
         SceneManager.LoadScene("Game");
         GameManager.m_NumRoundsToWin = int.Parse(roundText.text);
         GameManager.colors = colors;
@@ -118,7 +110,7 @@ public class MainMenu : MonoBehaviour {
         StartCoroutine(rotator(1));
     }
 
-    void calculateColors() {
+    private void calculateColors() {
         colors[0] = new Color(red1.value / 255, green1.value / 255, blue1.value / 255);
         colors[1] = new Color(red2.value / 255, green2.value / 255, blue2.value / 255);
     }
